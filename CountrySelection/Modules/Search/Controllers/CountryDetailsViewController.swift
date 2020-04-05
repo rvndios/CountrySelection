2010ed9552ab5d6e6a8a785e49e4298e5ea55c38
Aravind S <aravind.s@contus.in>
2020-04-05 11:01:05 +0530

    Multi language display with separator. support in detail page is added.

--- CountrySelection/Modules/Search/Controllers/CountryDetailsViewController.swift
+++ CountrySelection/Modules/Search/Controllers/CountryDetailsViewController.swift
@@ -18,13 +18,17 @@ class CountryDetailsViewController : UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setUpTexts()
+    }
+    /// Method to set texts to lables
+    func setUpTexts() {
         imageFlag.image(from: selectedData.flag, size: CGSize(width: 400, height: 400))//imageFlag.frame.size)
         labelCountryName.text = selectedData.name + " - " + selectedData.capital + " (+" + selectedData.callingCodes.joined(separator: ", ") + ")"
         labelRegion.text = selectedData.region + ", " + selectedData.subregion
         labelTimeZone.text = "Time Zone : " + selectedData.timezones.joined(separator: ", ")
-        let currency =  selectedData.currencies?.first
-        labelCurrency.text = "Currency: \( currency?.name ?? "") (\(currency?.symbol ?? ""))  - \(currency?.code ?? "")"
-        let language =  selectedData.languages?.first
-        labelLanguage.text = "Language: \( language?.name ?? "") (\(language?.iso639_1 ?? ""))"
+        labelCurrency.text = "Currency : " + (selectedData.currencies?.reduce(into: [String]()) {
+            return $0.append("\( $1.name ?? "") (\($1.symbol ?? ""))  - \($1.code ?? "")") }.joined(separator: ", ") ?? "")
+        labelLanguage.text = "Language : " + (selectedData.languages?.reduce(into: [String]()) {
+            return $0.append("\( $1.name ) (\($1.iso639_1 ?? "")) ") }.joined(separator: ", ") ?? "")
     }
 }
